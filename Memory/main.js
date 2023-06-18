731fdc4d5469de32e208ad873facdc14858970fd
UltBiginner <134677073+UltBiginner@users.noreply.github.com>
2023-06-18 17:45:24 +0900

    Update main.js

--- Memory/main.js
+++ Memory/main.js
@@ -164,4 +164,4 @@ const myArray = [
 'Osaka Expo'
 ];
 
-new PhotoViewer(myArray, '../images').init();
+new PhotoViewer(myArray, 'https://github.com/UltBiginner/my-PhotoViewer/blob/main/images').init();
